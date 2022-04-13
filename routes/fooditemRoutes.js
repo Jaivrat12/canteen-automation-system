c93b144986e2745585848b1bdfa88edfc13ac9cf
Vanshika Jain <vanshikajainvj2002@gmail.com>
2022-04-13 23:04:06 +0530

    added router files

---
+++ routes/fooditemRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { loginPost } = require('../controllers/loginContoller');
+const router = express.Router();
+
+router.post('/login', loginPost )
+
+module.exports = router;
+//
---
+++ routes/loginRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { loginPost } = require('../controllers/loginContoller');
+const router = express.Router();
+
+router.post('/login', loginPost )
+
+module.exports = router;
