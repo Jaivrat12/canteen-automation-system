50dfded2d4e37948d2f7f85208ad13c37c6298b9
Jaivrat Das <jaivratdas123456@gmail.com>
2022-04-14 21:43:58 +0530

    Updated example route handlers in menuRoutes.js

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -25,28 +25,31 @@ router.get('/:id', async (req, res) => {              // route handler for `GET`
 // add/create a new item
 router.post('/', async (req, res) => {                // route handler for `POST` requests to route `/menu/`
 
-    const newItem = new FoodItem({
+    const newItem = await FoodItem.create({
         name: req.body.name,
         price: req.body.price,
     });
-    const saved = await newItem.save();
-    res.redirect(`/menu/${ saved.id }`);
+    res.redirect(`/menu/${ newItem.id }`);
 });
 
 // update an item by id
-// or use `/:id/update`
-router.put('/:id', (req, res) => {              // route handler for `PUT` requests to route `/menu/id`
+// or use `/:id/edit`
+router.post('/:id/edit', async (req, res) => {              // route handler for `PUT` requests to route `/menu/id`
 
-    // res.render('index', { menu: menu });
+    // Still not final!!!
+    // const id = req.params.id;
+    // const updates = req.body;
+    // await FoodItem.update(updates, { where: { id: id } });
+    // res.redirect(`/menu/${ id }`);
 });
 
 // delete an item by id
 // or use `/:id/delete`
-router.delete('/:id', async (req, res) => {           // route handler for `DELETE` requests to route `/menu/id`
+router.post('/:id/delete', async (req, res) => {           // route handler for `DELETE` requests to route `/menu/id`
 
     const id = req.params.id;
     await FoodItem.destroy({ where: { id: id } });
-    res.send('/menu');
+    res.redirect('/menu');
 });
 
 module.exports = router;
