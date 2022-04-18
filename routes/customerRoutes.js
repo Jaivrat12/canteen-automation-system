9d2d063fb5b9611c364c8bbd51bc40ced952fcb7
Vanshika Jain <vanshikajainvj2002@gmail.com>
2022-04-18 23:08:32 +0530

    set cookies

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -17,18 +17,59 @@ router.get('/alerts', async(req, res) => {
 
 router.get('/cart', async(req, res) => {
 
-    const cart = {
-        items: [
-            { id: 1, name: 'abc', price: 123, quantity: 1 },
-            { id: 2, name: 'abc1', price: 124, quantity: 2 },
-            { id: 3, name: 'abc2', price: 125, quantity: 3 },
-        ],
-        total: 12345
+    const cart = JSON.parse(req.cookies.cart);
+
+    const itemIds = [];
+    cart.forEach((item) => {
+        itemIds.push(item.id);
+    })
+
+    const items = await FoodItem.findAll({
+        where: {
+            id: itemIds
+        }
+    });
+
+    let total = 0;
+    for (let i = 0; i < items.length; i++) {
+
+        items[i].quantity = cart[i].quantity;
+        total += Number(items[i].price) * cart[i].quantity;
     };
-    res.render('cart', cart);
+
+    res.render('cart', { items, total });
 });
 
 router.post('/cart', async(req, res) => {
+
+    const cart = JSON.parse(req.cookies.cart);
+
+    const itemIds = [];
+    cart.forEach((item) => {
+        itemIds.push(item.id);
+    })
+
+    const items = await FoodItem.findAll({
+        where: {
+            id: itemIds
+        }
+    });
+
+    let total = 0;
+    for (let i = 0; i < items.length; i++) {
+
+        items[i].quantity = cart[i].quantity;
+        total += Number(items[i].price) * cart[i].quantity;
+    };
+
+    const order = await Order.create({
+        status: 'pending',
+        customerId: req.session.userId
+    });
+
+    items.forEach(async (item) => {
+        await order.addFood_items(item, {through: {quantity: item.quantity}})
+    });
 
     res.redirect('/');
 });
