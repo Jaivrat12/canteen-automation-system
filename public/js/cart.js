af3b0bec799886469ac7454728aa5fc1dd5e9df6
Sakshi Telang <telangsakshi342@.com>
2022-04-18 15:59:56 +0530

    added Add to cart functionalities

---
+++ public/js/cart.js
@@ -0,0 +1,31 @@
+const cartBtns = document.querySelectorAll('.increment, .decrement');
+cartBtns.forEach(async (cartBtn) => {
+
+    cartBtn.addEventListener('click', async () => {
+
+        const itemQty = cartBtn.parentElement.children[1];
+        let qty = Number(itemQty.innerHTML);
+        if (cartBtn.getAttribute('op') === '+') {
+            qty++;
+        } else if (qty > 0) {
+            qty--;
+        }
+
+        itemQty.innerHTML = qty;
+        let cart = JSON.parse((await cookieStore.get('cart'))?.value || '[]');
+        console.log(cart);
+        const itemId = itemQty.getAttribute('item-id');
+        const item = cart.find(item => item.id === itemId);
+        if (item) {
+
+            if (qty === 0) {
+                cart = cart.filter(item => item.id !== itemId);
+            } else {
+                item.quantity = qty;
+            }
+        } else {
+            cart.push({ id: itemId, quantity: qty });
+        }
+        await cookieStore.set('cart', JSON.stringify(cart));
+    });
+});
