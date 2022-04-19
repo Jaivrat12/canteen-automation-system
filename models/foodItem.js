8fb779fd80ca7732469fc694383a3314e150920f
Jaivrat Das <jaivratdas123456@gmail.com>
2022-04-19 21:23:20 +0530

    updated db models

--- models/foodItem.js
+++ models/foodItem.js
@@ -7,16 +7,16 @@ const FoodItem = db.define('food_item', {
     id: myTypes.id,
     name: myTypes.text(),
     price: {
-        type: DataTypes.DECIMAL(6, 2),
+        type: DataTypes.DECIMAL(10, 2),
         allowNull: false
     },
-    category: myTypes.text(),
     isAvailable: {
         type: DataTypes.BOOLEAN,
         defaultValue: true,
         allowNull: false
     },
-    image: DataTypes.BLOB,
+    image: DataTypes.TEXT,
+    category: DataTypes.TEXT,
     description: DataTypes.TEXT,
     prepTime: DataTypes.TIME
 }, {
